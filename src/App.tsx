ec231e1c1b721ec8d6da489d6f679e04756037f7
Rishik Chand <rishik01@uw.edu>
2025-12-24 13:47:39 -0800

    made landingPage less ugly

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import LandingPage from "./pages/landingPage";
--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -1,10 +1,19 @@
 import {Link} from "react-router-dom"
+import "./landingPage.css"
 
-export default function landingPage() {
+export default function LandingPage() {
     return (
-        <div>
-            <h1>Home page!</h1>
-            <Link to="/blog">Go to Blog</Link>
+        <div className="landing-container">
+            <div className="hero">
+                <h1 className="hero-title">RSS react project</h1>
+                <p className="hero-subtitle">
+                    A collection of some Medium blogs
+                </p>
+
+                <Link to="/blog" className="hero-button">
+                    View Blog
+                </Link>
+            </div>
         </div>
-    )
+    );
 }
