e5bbb6e1ed621810f448bb2f25bcb9d14a3c2b14
Rishik Chand <rishik01@uw.edu>
2025-12-28 12:01:24 -0800

    blogPage setup

--- src/pages/blogPage.tsx
+++ src/pages/blogPage.tsx
@@ -1,10 +1,26 @@
-import { Link } from "react-router-dom";
+import BlogCard from "../components/blogCard";
+import "./blogPage.css";
 
 export default function BlogPage() {
+    const posts = [
+        {
+            title: "meow meow",
+            author: "car",
+            date: "Jan 77, 2024",
+            description: "meow emwo emow",
+            link: "https://medium.com",
+        },
+    ];
+
     return (
-        <div>
-            <h1>Blogs</h1>
-            <Link to="/">Back Home</Link>
+        <div className="blog-page-container">
+            <h1 className="blog-page-title">Blog</h1>
+
+            <div className="blog-grid">
+                {posts.map((post, index) => (
+                    <BlogCard key={index} {...post} />
+                ))}
+            </div>
         </div>
     );
 }
